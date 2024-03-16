268eab5d57626a140476d8536fc996d7faaf2386
unknown <hassanwebdeveloper8322@gmail.com>
2024-03-16 17:45:35 +0500

    first commit

--- db/connect.js
+++ db/connect.js
@@ -1,20 +1,10 @@
-// const mongoose = require('mongoose')
-
-// const connectDB = (url) => {
-//   return mongoose.connect(url, {
-//     useNewUrlParser: true,
-//     useCreateIndex: true,
-//     useFindAndModify: false,
-//     useUnifiedTopology: true,
-//   })
-// }
-
-// module.exports = connectDB
+require('dotenv').config();
 const mongoose = require('mongoose');
-mongoose.set('strictQuery', true);
-
-const connectDB = (url) => {
-  return mongoose.connect(url);
+const connectDB = () => {
+  return (
+    mongoose.set("strictQuery", false),
+    mongoose.connect("mongodb+srv://Hassan:Hassan12345@nodejsexpressjs.yqtqanv.mongodb.net/AKAZA-API?retryWrites=true&w=majority")
+  )
 };
 
 module.exports = connectDB;
